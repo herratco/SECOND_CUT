6d71a2382afa0d8d83c414e322bffb408196a2df
herratco <110746683+herratco@users.noreply.github.com>
2023-10-06 16:06:03 +0200

    Delete src directory

--- src/components/cart/index.jsx
+++
@@ -1,6 +0,0 @@
-import Cart from "./Cart";
-// import withAuth from "../helper/withAuth";
-
-const CartIndex = () => <Cart />;
-
-export default CartIndex;
--- src/components/catagories/index.jsx
+++
@@ -1,5 +0,0 @@
-import SingleCategory from "./Categories";
-
-export default function Category() {
-	return <SingleCategory />;
-}
